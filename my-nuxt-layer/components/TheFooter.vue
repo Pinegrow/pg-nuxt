8fff0a5a4b516821ea2a4f7adeec96cad7139125
Akayy <techakayy@gmail.com>
2024-06-06 15:00:50 +1000

    Initial commit

---
+++ my-nuxt-layer/components/TheFooter.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+  //...
+</script>
+<template>
+  <footer class="pb-24 pt-12">
+    <div class="container mx-auto px-8" style="">
+      <div class="flex flex-wrap justify-center space-x-8">
+        <NuxtLink
+          href="https://www.facebook.com/vuedesigner"
+          aria-label="facebook"
+          external
+          target="_blank"
+        >
+          <BaseIcon
+            name="i-logos-facebook"
+            class="text-2xl"
+            width="28px"
+          ></BaseIcon>
+        </NuxtLink>
+        <NuxtLink
+          href="https://twitter.com/vuedesigner"
+          aria-label="twitter"
+          external
+          target="_blank"
+        >
+          <BaseIcon
+            name="i-logos-twitter"
+            class="text-2xl"
+            width="28px"
+          ></BaseIcon>
+        </NuxtLink>
+        <NuxtLink
+          href="https://discord.gg/BYp45Nnu5T"
+          aria-label="discord"
+          external
+          target="_blank"
+        >
+          <BaseIcon
+            name="i-logos-discord-icon"
+            class="text-2xl"
+            width="28px"
+          ></BaseIcon>
+        </NuxtLink>
+        <NuxtLink
+          href="https://www.youtube.com/@vuedesigner"
+          aria-label="youtube"
+          external
+          target="_blank"
+        >
+          <BaseIcon
+            name="i-logos-youtube-icon"
+            class="text-2xl"
+            width="28px"
+          ></BaseIcon>
+        </NuxtLink>
+      </div>
+    </div>
+  </footer>
+</template>
+<style scoped>
+  .pb-24 {
+    padding-bottom: 6rem /* 96px */;
+  }
+  .pt-12 {
+    padding-top: 3rem /* 48px */;
+  }
+
+  .container {
+    width: 100%;
+  }
+  @media (min-width: 640px) {
+    .container {
+      max-width: 640px;
+    }
+  }
+  @media (min-width: 768px) {
+    .container {
+      max-width: 768px;
+    }
+  }
+  @media (min-width: 1024px) {
+    .container {
+      max-width: 1024px;
+    }
+  }
+  @media (min-width: 1280px) {
+    .container {
+      max-width: 1280px;
+    }
+  }
+  @media (min-width: 1536px) {
+    .container {
+      max-width: 1536px;
+    }
+  }
+  .mx-auto {
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .px-8 {
+    padding-left: 2rem /* 32px */;
+    padding-right: 2rem /* 32px */;
+  }
+  .flex {
+    display: flex;
+  }
+  .flex-wrap {
+    flex-wrap: wrap;
+  }
+  .justify-center {
+    justify-content: center;
+  }
+  .space-x-8 > :not([hidden]) ~ :not([hidden]) {
+    --tw-space-x-reverse: 0;
+    margin-right: calc(2rem /* 32px */ * var(--tw-space-x-reverse));
+    margin-left: calc(2rem /* 32px */ * calc(1 - var(--tw-space-x-reverse)));
+  }
+  .text-2xl {
+    font-size: 1.5rem /* 24px */;
+    line-height: 2rem /* 32px */;
+  }
+</style>
---
+++ my-nuxt-layer/components/TheLogo.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+  import site from '@/site'
+  const { name, logo } = site
+</script>
+<template>
+  <NuxtLink data-pg-name="Logo" class="flex items-center sm:flex-row" to="/">
+    <BaseIcon height="40" class="mt-2" :name="logo" />
+    <h6
+      class="text-primary-600 dark:text-primary-200 font-extrabold font-serif ml-2 mt-4 text-2xl"
+    >
+      {{ name }}
+    </h6>
+  </NuxtLink>
+</template>
+<style scoped>
+  .flex {
+    display: flex;
+  }
+  .items-center {
+    align-items: center;
+  }
+  @media (min-width: 640px) {
+    .sm\:flex-row {
+      flex-direction: row;
+    }
+  }
+  .mt-2 {
+    margin-top: 0.5rem /* 8px */;
+  }
+  .text-primary-600 {
+    --tw-text-opacity: 1;
+    color: rgb(180 100 48 / var(--tw-text-opacity));
+  }
+  :is(.dark .dark\:text-primary-200) {
+    --tw-text-opacity: 1;
+    color: rgb(229 191 170 / var(--tw-text-opacity));
+  }
+  .font-extrabold {
+    font-weight: 800;
+  }
+  .font-serif {
+    font-family: 'Sora', 'Sora fallback', sans-serif;
+  }
+  .ml-2 {
+    margin-left: 0.5rem /* 8px */;
+  }
+  .mt-4 {
+    margin-top: 1rem /* 16px */;
+  }
+  .text-2xl {
+    font-size: 1.5rem /* 24px */;
+    line-height: 2rem /* 32px */;
+  }
+</style>
