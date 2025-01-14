f5b96dfebdb8976fb9f328b21130edcb820dcad5
Akayy <techakayy@gmail.com>
2025-01-14 20:19:08 +1100

    Updates, navbar refactor and head-and-meta.ts

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,18 @@
-<script lang="ts" setup>
-  useHeadAndMeta()
+<script setup lang="ts">
+  const route = useRoute()
+
+  const pageMeta = computed(() => {
+    return {
+      title: route.meta.title,
+      description: route.meta.description,
+      ogImage: route.meta.ogImage,
+      canonicalUrl: route.meta.canonicalUrl || route.fullPath,
+      generator: route.meta.generator,
+      tags: route.meta.tags,
+    }
+  })
+
+  useHeadAndMeta(pageMeta)
   useOgImage()
 </script>
 
